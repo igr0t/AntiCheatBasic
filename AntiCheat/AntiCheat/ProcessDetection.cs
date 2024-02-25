5f393e4c4d27ea28e27e3f425b5617a6da8c9cb2
ig$ <114263816+igrlq@users.noreply.github.com>
2024-02-25 14:13:36 -0300

    Add files via upload

---
+++ AntiCheat/AntiCheat/ProcessDetection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AntiCheat
+{
+
+
+
+    internal class ProcessDetection
+    {
+        public static bool detected;
+        public static List<string> currentlyRunning = new List<string>();
+        public static string[] suspiciousNames = { "Cheat", "Debugger", "x64dbg", "IDA PRO", "Cheat Engine", "cheatengine", "x32dbg", "x64dbg (32bits)" };
+        public static string ignore = AppDomain.CurrentDomain.FriendlyName; // Lista de Permitidos
+
+        public static void UpdateProcList()
+        {
+            Process[] running = Process.GetProcesses();
+            foreach (Process proc in running)
+            {
+                currentlyRunning.Add(proc.ProcessName);
+
+            }
+        }
+         public static void FindProcess()
+        {
+            foreach(string procName in currentlyRunning)
+            {
+                for(int i = 0; i < suspiciousNames.Length; i++)
+                {
+                    if (procName != ignore)
+                    {
+                        if (procName.Contains(suspiciousNames[i]))
+                        {
+                            detected = true;
+                            Console.WriteLine("Detectado:" + suspiciousNames[i]);
+                            return;
+                        }
+                    }
+
+                }
+            }
+                    }
+
+    }
+    
+}
---
+++ AntiCheat/AntiCheat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace AntiCheat
+{
+    class Program
+    {
+        private static Thread thread;
+        static void Main(string[] args)
+        {
+            thread = new Thread(ConsoleThread);
+            thread.Name = "ConsoleThread";
+            thread.Start();
+            Console.ReadLine();
+        }
+        public static void ConsoleThread()
+        {
+            while (true)
+            {
+                ProcessDetection.UpdateProcList();
+                ProcessDetection.FindProcess();
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
